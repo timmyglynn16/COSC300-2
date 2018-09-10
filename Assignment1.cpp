707413bd08fe9a21f520b5ffabc57f5b217a8529
tglynn <timmyglynnv3@gmail.com>
2018-09-09 22:44:11 -0400

    cpp program

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,8 +1,93 @@
 #include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <iomanip>
 using namespace std;
 
-int main() 
-{
-    cout << "this is another test " << endl;
-    return 0;
+
+void OddNums(){
+
+    cout << "Print Odd Numbers from 1 to 10001";
+    cout << "\n\n";
+    for (int a=1; a<=10001; a+=2) {
+     
+        cout << " " << a << " ";\
+        if ((a % 20) == 9) // print 10 lines per line
+        cout << endl;
+    }
+
+    cout << endl;
+
+}
+
+void CalenderOps(){
+
+    cout << "__________________________" << endl;
+    cout << "Calender Conversions:";
+    cout << endl;
+
+    cout << "Enter how many days:" << endl;
+    double day;
+    cin >> day;
+
+    double year = day/365.0;
+    double month = ceil(day * 0.032855); //ceil rounds up the value
+
+    cout << "Days: " << day << endl;
+    cout << "Years: " << year << endl;
+    cout << "Months: " << month << endl; //value for more understandable answer
+
+}
+
+void FindMax() {
+    cout << "__________________________" << endl;
+    cout << "Find Max:";
+    cout << endl;
+
+    cout << "Enter 3 integers: " << endl;
+    double num1;
+    double num2;
+    double num3;
+
+    cin >> num1 >> num2 >> num3;
+    cout << endl;
+
+	int max = num1; /* assume x is the largest */
+
+	if (num2 > max) { /* if y is larger than max, assign y to max */
+		max = num2;
+	} /* end if */
+
+	if (num3 > max) { /* if z is larger than max, assign z to max */
+		max = num3;
+	} /* end if */
+
+	cout << "The max integer is: " << max << endl;
+
+}
+
+void ConvertTemp(){
+    cout << "__________________________" << endl;
+    cout << "Temperature Conversion: ";
+    cout << endl;
+
+    cout << "Enter your temperature in Fahrenheit" << endl;
+    
+    double tempF;
+    cin >> tempF;
+
+    double tempC = (tempF-32.0)*(5.0/9.0);
+
+    cout << "Temp (F) to Temp (C): ";
+    cout << fixed << setprecision(2) << tempC << endl; 
+
+}
+
+//main has to be called last??
+int main() {
+    //std::cout << "Main was reached";
+    OddNums();
+    CalenderOps();
+    FindMax();
+    ConvertTemp();
 }
--- Assignment1.java
+++ Assignment1.java
@@ -7,24 +7,29 @@ public class Assignment1 {
 
     public static void main(String[] args) {
 
-       //OddNums();
-       //CalenderOps();
+       OddNums();
+       CalenderOps();
        FindMax();
+       ConvertTemp();
         
 
     }
 
     public static void OddNums(){
+        System.out.println("__________________________");
+        System.out.println("Print Odd Nums:");
         System.out.println("The Odd Numbers are:");
 		for (int i = 1; i <= 1001; i++) {
 			if (i % 2 != 0) {
 				System.out.print(i+ ((i%20==9) ? "\n" : " "));
 			}
-		}
+        }
+        System.out.println();
     }
 
     public static void CalenderOps(){
-        
+        System.out.println("__________________________");
+        System.out.println("Calender Conversions:");
         System.out.println("Enter number of days");
         double days = input.nextInt();
 
@@ -38,22 +43,40 @@ public class Assignment1 {
 
         //conver to WHAT??
 
+        System.out.println();
     }
 
     public static void FindMax(){
+        System.out.println("__________________________");
+        System.out.println("Find Max:");
         System.out.println("Enter 3 integers: ");
         int num1 = input.nextInt();
         int num2 = input.nextInt();
         int num3 = input.nextInt();
         
         int max = Math.max(num1,num2);
-        if(max>num2){ //suppose x is max then compare x with z to find max number
+        if(max>num2){
             max = Math.max(num1,num3);    
-        }
-        else{ //if y is max then compare y with z to find max number
+       }
+        else { 
             max = Math.max(num2,num3);    
-        }
+     }
         System.out.println("The max integer is: " + max);
+        System.out.println();
+    }
+
+    public static void ConvertTemp(){
+        System.out.println("__________________________");
+        System.out.println("Temperature Conversion:");
+        System.out.println("Enter your degrees in Fahrenheit");
+        double tempF = input.nextDouble();
+
+        double tempC = (tempF-32.0)*(5.0/9.0);
+
+        System.out.println("Temp (F) to Temp (C): ");
+        System.out.printf("%.2f", tempC);
+        //System.out.printf("Temp (F) to Temp (C): " + tempC);
+        System.out.println();
 
     }
 }
