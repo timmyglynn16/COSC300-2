0104a078376ca44eaa05367f58b7bb9a9db89684
tglynn <timmyglynnv3@gmail.com>
2018-09-24 18:07:48 -0400

    more push

--- Assignment2/Part1/insertSort.java
+++ Assignment2/Part1/insertSort.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
 
+public static void main(String[] args) {
+    
+    int[] a = { 7, 8, 9, 10, 1, 6, 2, 4, 3, 5};
+    insertSort(a, a.length);
+    
+    for(int i : a)
+    System.out.print(i + " ");
+    System.out.println();
+}
+
 static void insertSort(int[] a, int num) {
     
     for(int j=1; j<num; j++) {
@@ -18,10 +28,3 @@ static void insertSort(int[] a, int num) {
             
 }
 
-public static void main(String[] args) {
-int[] a = { 7, 8, 9, 10, 1, 6, 2, 4, 3, 5};
-insertSort(a, a.length);
-for(int i : a)
-System.out.print(i + " ");
-System.out.println();
-}
