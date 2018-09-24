b0bae9b31eed6a86ee06f56e2374bf62fbd8f88a
tglynn <timmyglynnv3@gmail.com>
2018-09-24 18:48:50 -0400

    3 push

--- Assignment2/Part1/insertSort3.cpp
+++ Assignment2/Part1/insertSort3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>  // for high_resolution_clock
 using namespace std;
 
 
@@ -19,8 +20,20 @@ void insertSort3(int* low, int* high) {
 int main(void) {
     int a[] = { 7, 8, 9, 10, 1, 6, 2, 4, 3, 5};
     int *low = a, *high = a + sizeof(a)/sizeof(a[0]) - 1;
+
+    // Record start time
+    auto start = std::chrono::high_resolution_clock::now();
+
+    // Portion of code to be timed
     insertSort3(low, high);
+    
+    // Record end time
+    auto finish = std::chrono::high_resolution_clock::now();
+    
     for(int *p=low; p<=high; p++)
         cout << *p << " ";
         cout << endl;
+    
+    std::chrono::duration<double> elapsed = finish - start;
+    std::cout << "Elapsed time: " << elapsed.count() << " s\n"; 
 }
