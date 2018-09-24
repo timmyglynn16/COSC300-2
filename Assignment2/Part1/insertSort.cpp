432fac9bc0b236516ec9beb43ddaf30ffc5d7aa4
tglynn <timmyglynnv3@gmail.com>
2018-09-24 18:33:20 -0400

    cpp first sort

---
+++ Assignment2/Part1/insertSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void insertSort(int a[], int num) {
+    for(int j=1; j<num; j++) {
+        int t = a[j], i = j;
+            
+        while(i > 0 && a[i-1] > t) {
+            a[i] = a[i-1];
+            i--;
+                
+        }
+                
+            a[i] = t;
+        
+    }
+}
+    
+int main(void) {
+    int a[] = { 7, 8, 9, 10, 1, 6, 2, 4, 3, 5};
+    insertSort(a, sizeof(a)/sizeof(a[0]));
+    for(int i=0; i<sizeof(a)/sizeof(a[0]); i++)
+    cout << a[i] << " ";
+    cout << endl;
+        
+}
+
+    
+
+
