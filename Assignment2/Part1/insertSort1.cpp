238dc535648b7e0c54b1f60204ed5ff2922bdbbe
tglynn <timmyglynnv3@gmail.com>
2018-09-24 18:45:10 -0400

    add clock

--- Assignment2/Part1/insertSort1.cpp
+++ Assignment2/Part1/insertSort1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>  // for high_resolution_clock
 using namespace std;
 
 void insertSort1(int a[], int num) {
@@ -16,11 +17,21 @@ void insertSort1(int a[], int num) {
 int main(void) {
     int a[] = { 7, 8, 9, 10, 1, 6, 2, 4, 3, 5};
     
+    // Record start time
+    auto start = std::chrono::high_resolution_clock::now();
+
+    // Portion of code to be timed
     insertSort1(a, sizeof(a)/sizeof(a[0]));
+    
+    // Record end time
+    auto finish = std::chrono::high_resolution_clock::now();
+    
     for(int i=0; i<sizeof(a)/sizeof(a[0]); i++)
     cout << a[i] << " ";
     cout << endl;
-     
+
+    std::chrono::duration<double> elapsed = finish - start;
+    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
 }
 
     
