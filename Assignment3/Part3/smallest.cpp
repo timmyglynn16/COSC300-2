1a140eb6be9eda9be7f8c66724ccc243dc1bc44b
tglynn <timmyglynnv3@gmail.com>
2018-10-02 22:00:26 -0400

    okay

---
+++ Assignment3/Part3/smallest.cpp
@@ -0,0 +1,73 @@
+#include <iostream> 
+using namespace std; 
+  
+// recursive function 
+int smallest(int a[], int lo, int hi) 
+{ 
+    //int midpoint = (hi - lo) / 2;
+    //int min_left = smallest(a, lo, midpoint);
+    //int min_right = smallest(a, midpoint+1, hi);
+
+    /*
+    if(min_left < min_right)
+    return min_left;
+
+    if(min_right < min_left)
+    return min_right; */
+
+    if (lo >= hi -1) {
+        return lo;
+    }
+
+    else {
+        int newLo = smallest(a, lo + 1, hi);
+    
+    if (a[lo] <= a[newLo]) {
+        return lo;
+    }
+
+        else {
+            return newLo;
+        }
+    }
+
+    //return min(smallest(a, lo-1, hi+1));
+
+    /*
+    if (lo == 1) 
+        return a[0]; */
+    
+    //return min(a[0], smallest(a, lo+1, hi-1)); 
+} 
+
+int smallest(int a[], int n) {
+    return smallest(a, 0, n);
+}
+
+int main() 
+{ 
+
+    int n, i;
+    int a[n];
+
+    cout << "\nEnter the size of the array: ";
+    cin >> n;
+    cout << endl;
+
+    // enter integers into the array
+    for(i = 0; i < n; i++) 
+    { 
+        cout << "Enter element " << i+1 << ": "; 
+        cin >> a[i]; 
+    } 
+
+    // set hi and lo equal to the length of the array 
+    // sizeof(a) returns the number of BYTES the array occupies
+    // divide by the first element of the array to get the number of ELEMENTS in the array
+    //int lo = a[0]; 
+    //int hi = a[i++]; 
+    int lo = a[0]; 
+    int hi = a[n-1]; 
+    cout <<  smallest(a, n); 
+    return 0; 
+} 
